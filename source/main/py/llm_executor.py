29fbab4a5317aee9cda51b8cc68d286ae9499d93
Pablo <edu@pablotech.uno>
2023-10-31 10:50:19 -0700

    execution classes

--- source/main/py/llm_executor.py
+++ source/main/py/llm_executor.py
@@ -163,7 +163,7 @@ class PipelinedExecutor():
             try:
                 agent_step, observation = None, None
                 self.context_values.set_history(self.llm_agent.get_memory().__str__())
-                self.context_values.set_scratchpad(self.execution_journey)
+                self.context_values.set_scratchpad(self.execution_journey.__str__())
                 agent_step = self.llm_agent.invoke(self.context_values)
 
                 if isinstance(agent_step, AgentAction):
