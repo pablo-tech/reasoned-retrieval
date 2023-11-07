ec8e081df21d70411950494288e17a5521ae4b59
Pablo <edu@pablotech.uno>
2023-11-07 09:12:01 -0800

    adopted Reader Retriever nomenclature

--- source/main/py/tool_hotpot.py
+++ source/main/py/tool_hotpot.py
@@ -9,7 +9,7 @@ class HotpotDataset(ToolSelect):
     # https://pypi.org/project/wikipedia/
 
     def __init__(self, completion_llm, is_verbose):
-        super().__init__("HOTPOT")
+        super().__init__("HOTPOT", completion_llm, is_verbose)
         self.completion_llm = completion_llm
         self.is_verbose = is_verbose
         hotpot_train = '/content/drive/MyDrive/StanfordLLM/hotpot_qa/hotpot_train_v1.1.json'
