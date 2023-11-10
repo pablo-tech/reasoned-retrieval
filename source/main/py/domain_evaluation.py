58296f534aca771b912d7682f8d9b01c8d8bec2e
Pablo <edu@pablotech.uno>
2023-11-10 01:02:47 -0800

    easy to lookup product with narrative fields

--- source/main/py/domain_evaluation.py
+++ source/main/py/domain_evaluation.py
@@ -5,7 +5,6 @@ class HotpotDataset():
     # https://pypi.org/project/wikipedia/
 
     def __init__(self, completion_llm, is_verbose):
-        super().__init__("HOTPOT", completion_llm, is_verbose)
         self.completion_llm = completion_llm
         self.is_verbose = is_verbose
         hotpot_train = '/content/drive/MyDrive/StanfordLLM/hotpot_qa/hotpot_train_v1.1.json'
