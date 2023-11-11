bcd6b8d00cf1a932d86610f3f40d4330f1f24175
Pablo <edu@pablotech.uno>
2023-11-11 05:10:46 -0800

    multi model hotpot tracing

--- source/main/py/model_trace.py
+++ source/main/py/model_trace.py
@@ -11,34 +11,31 @@ class ThoughtTracer():
     def __init__(self, is_verbose):
         self.is_verbose = is_verbose
 
-    def thought_traces(self, name, llm, tools, data, n):
-        question_traces = {}
+    def thought_traces(self, named_llms, tools, data, n):
+        traces = defaultdict(dict)
 
         for i in range(n):
             example = data[i]
             question = example['question'].strip()
             correct_answer = example['answer'] 
             try:
-                bot = ChatBot(agent_llm=llm,
-                                agent_tools=tools,
-                                is_verbose=self.is_verbose)
-                inferred_response = bot.invoke(question)
-                question_traces[question] = inferred_response
-                width=75  
-                print(textwrap.fill(str(question), width))
-                print(textwrap.fill("CORRECT=" + correct_answer, width)) 
-                print(textwrap.fill(name + " " + "INFERRED=" + inferred_response.get_answer(), width))
+                for name, llm in named_llms.items():
+                    bot = ChatBot(agent_llm=llm,
+                                  agent_tools=tools,
+                                  is_verbose=self.is_verbose)
+                    inferred_response = bot.invoke(question)
+                    traces[name][question] = inferred_response
+                    width=75  
+                    print(textwrap.fill("\n--> " + str(question), width))
+                    print(textwrap.fill("CORRECT=" + correct_answer, width)) 
+                    print(textwrap.fill(name + " " + "INFERRED=" + inferred_response.get_answer(), width))
 
             except Exception as e:
                 print("QUESTIONER_ERROR="+str(e)+"..."+str(question))
                 print("\n")
                 pass
-        return question_traces  
+        return traces  
 
     def hotpot_traces(self, named_llms, data, n):
-        model_traces = defaultdict(list)
-        for name, llm in named_llms.items():
-            tools = HotpotToolFactory(llm).get_tools()
-            question_traces = self.thought_traces(name, llm, tools, data, n)
-            model_traces[name].add(question_traces)
-        return model_traces
+        tools = HotpotToolFactory(named_llms).get_tools()
+        return self.thought_traces(named_llms, tools, data, n)
