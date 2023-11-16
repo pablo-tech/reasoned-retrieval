1ba32709c4969860192d6567489862445894bb0d
Pablo <edu@pablotech.uno>
2023-11-16 13:45:52 -0800

    two loaders

--- source/main/py/helper_sql.py
+++ source/main/py/helper_sql.py
@@ -26,49 +26,51 @@ class RunInference():
 class SqlSemanticParser(RunInference):
 
     def __init__(self, db_cursor,
-                 query_columns, schema_signature, query_enums,
+                 query_columns, schema_name, schema_signature, query_enums,
                  completion_llm, is_verbose=True):
         super().__init__(completion_llm, is_verbose)
         self.db_cursor = db_cursor
         self.query_columns = query_columns
+        self.schema_name = schema_name
         self.schema_signature = schema_signature
         self.query_enums = query_enums
 
     def invoke(self, query):
         prompt = self.get_prompt(query, 
                                  self.query_columns,
+                                 self.schema_name,
                                  self.schema_signature,
                                  self.query_enums)
         inferred_sql = self.run_inference(prompt)
         response = self.db_cursor.execute(inferred_sql)
         return [row for row in response]
             
-    def get_prompt(self, question, columns, signature, enums):
+    def get_prompt(self, question, columns, schema_name, table_signature, enums):
         prompt = "You are an AI expert semantic parser."
         prompt += "Your task is to generate a SQL query string for the provided question." + "\n"
         prompt += f"The only table columns to return are {columns}"
         prompt += "The database to generate the SQL for has the following signature: " + "\n"  
-        prompt += f"{signature}" 
+        prompt += f"{table_signature}" 
         prompt += "Note that table columns take the following enumerated values:" + "\n"
         for column, values in enums:
             prompt += f"{column} => {values}" + "\n"
         prompt += "Importantly, you must adjust queries for any possible question mispellings."
         prompt += "EXAMPLES:" + "\n"
-        prompt += """        
+        prompt += f"""        
 Question: what ARISTOCRAT products do you have? 
-Answer: SELECT brands, price, title FROM CLIQ WHERE brands = 'Aristocrat';
+Answer: SELECT brands, price, title FROM {schema_name} WHERE brands = 'Aristocrat';
 Question: what GESTS products do you have?
-Answer: SELECT brands, price, title FROM CLIQ WHERE brands = 'Guess';
+Answer: SELECT brands, price, title FROM {schema_name} WHERE brands = 'Guess';
 Question: what are the cheapest Scharf products?
-Answer: SELECT brands, price, title FROM CLIQ WHERE brands = 'Scharf' ORDER BY price ASC;
+Answer: SELECT brands, price, title FROM {schema_name} WHERE brands = 'Scharf' ORDER BY price ASC;
 Question: "what are the cheapest Carpisa watches?"
-Answer: SELECT brands, price, title FROM CLIQ WHERE brands = 'Carpisa' AND title LIKE '%watch%' ORDER BY price ASC;
+Answer: SELECT brands, price, title FROM {schema_name} WHERE brands = 'Carpisa' AND title LIKE '%watch%' ORDER BY price ASC;
 Question: "What is GW0403L2?"
-Answer: SELECT brands, price, title FROM CLIQ WHERE title LIKE '%GW0403L2%';
+Answer: SELECT brands, price, title FROM {schema_name} WHERE title LIKE '%GW0403L2%';
 Question: "Bags for men?"
-Answer: SELECT brands, price, title FROM CLIQ WHERE title LIKE '%bag%' AND title NOT LIKE '%women%';
+Answer: SELECT brands, price, title FROM {schema_name} WHERE title LIKE '%bag%' AND title NOT LIKE '%women%';
 Question: "Glassses for women?"
-Answer: SELECT brands, price, title FROM CLIQ WHERE title LIKE '%glass%' AND title NOT LIKE '% men%';
+Answer: SELECT brands, price, title FROM {schema_name} WHERE title LIKE '%glass%' AND title NOT LIKE '% men%';
 
 """
         prompt += f"Question: {question}" + "\n"
@@ -85,7 +87,6 @@ class SummaryTagger(RunInference):
             
     def invoke(self, products):
         product_summaries= []
-        # product_summary = defaultdict(dict)
         summary_values = defaultdict(set)
         i = 0
         for product in products:
@@ -101,7 +102,6 @@ class SummaryTagger(RunInference):
                 tag = summary_value[0]
                 value = summary_value[1]
                 product_summary[tag] = value
-                # product_summary[product[self.primary_key]][tag] = value
                 summary_values[tag].add(value)
                 if self.is_verbose:
                     print(str(i) + "/" + str(len(products)) + "\t" + "summary_value="+str(summary_value))
