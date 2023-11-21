d730367cd3c332b8960114cebb694303f5a7914c
Pablo <edu@pablotech.uno>
2023-11-21 09:52:58 -0800

    load faq and files to vector store

--- source/main/py/tool_vector.py
+++ source/main/py/tool_vector.py
@@ -8,14 +8,43 @@ from helper_select import SelectHelper
 class VectorRetriever(SelectHelper):
     # https://pypi.org/project/wikipedia/
 
-    def __init__(self, completion_llm, is_verbose=False):
+    def __init__(self, 
+                 completion_llm, 
+                 is_verbose=False):
         super().__init__("PINECONE", completion_llm, is_verbose)
         self.completion_llm = completion_llm
         self.is_verbose = is_verbose
         self.doc_store = PineconeDb(index_name="quickstart",
                                     is_create=True)
+        self.load_faq()
+        self.load_docs()
         print(self.doc_store.__str__())
 
+    def load_faq(self,
+                 files=['joined_faq.csv'], 
+                 path='/content/drive/MyDrive/StanfordLLM/qa_data/faq_qa/',):
+        docs = self.doc_store.read_files(file_names=files,
+                                             directory_path=path)
+        begins = [" ".join(doc.page_content.split(" ")[:3]).lstrip('\"') 
+                for doc in docs]
+        metas = [{'begins':str(begin)} for begin in begins]        
+        self.doc_store.load_docs(items=docs, metas=metas)        
+        
+    def load_docs(self,
+                  files=['pay-user.txt',
+                        'payoff-sale.txt',
+                        'refer-and-earn.txt',
+                        'sale-parade.txt',
+                        'savings-calculator.txt',
+                        'seller-terms.txt',
+                        'tdl-privacy.txt',
+                        'terms-conditions.txt',
+                        'tpl-privacy.txt'],
+                   path='/content/drive/MyDrive/StanfordLLM/qa_data/legal_qa/'):
+        docs = self.doc_store.read_files(files, path)        
+        metas = []
+        self.doc_store.load_docs(items=docs, metas=metas)  
+              
 
 class VectorSearchReader(VectorRetriever):
 
