170b54ee4e8a35389cf19cd551d68c59dbbc9e7f
Pablo <edu@pablotech.uno>
2023-11-24 11:47:33 -0800

    response object

--- source/main/py/helper_suql.py
+++ source/main/py/helper_suql.py
@@ -245,8 +245,8 @@ ON context.id = inference.id
         # return { **self.context_parser.get_enum_values(), 
         #          **self.inference_parser.get_enum_values() }
 
-    def get_fewshot_examples(self, filter_columns):
-        columns = ", ".join(self.get_columns(filter_columns))
+    def get_fewshot_examples(self):
+        columns = ", ".join(self.get_columns())
         return f"""        
 Question: what backpacks do you have? 
 Answer: SELECT {columns} FROM {self.get_table_name()} WHERE inference.product_types = 'backpack';
