669f8bbe2520bb2712d0b53c5ba12dafdf08e517
Pablo <edu@pablotech.uno>
2023-11-26 01:52:15 -0800

    save summary

--- source/main/py/helper_suql.py
+++ source/main/py/helper_suql.py
@@ -89,7 +89,7 @@ class DatasetLoader(SchemaCreator):
         # print("PRODUCTS=>" + str(products))
         print("COLUMNS=>" + str(columns))
         rows = DataTransformer.product_strs(products, columns, self.primary_key)
-        for chunk in rows.split("\n"):
+        for chunk in rows.split("\n")[:3]:
             print("ROW=>" + str(chunk))
         insert_sql = self.get_sql(self.table_name, rows)
         return insert_sql
