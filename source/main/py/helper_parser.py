d0afc18c7e0414641ef8b41959a05e75128f2bf6
Pablo <edu@pablotech.uno>
2023-11-28 08:56:15 -0800

    commerce tool

--- source/main/py/helper_parser.py
+++ source/main/py/helper_parser.py
@@ -128,8 +128,8 @@ class SqlSemanticParser(RunInference):
         return self.db_cursor.execute(query)
     
     def invoke_context(self, query, n):
-        invocations = self.domain_oracle.get_context_parser().get_invocations()
-        return self.invoke(query, n, invocations)
+        invocation = self.domain_oracle.get_context_parser().get_invocations()
+        return self.invoke(query, n, [invocation])
 
     def invoke_subdomain_inference(self, query, n, subdomain_name):
         invocations = self.domain_oracle.get_inference_parser().subdomain_invocations(subdomain_name)
