8eaed57f6596545d1762fb2bbfebb83214351969
Pablo <edu@pablotech.uno>
2023-11-29 07:25:52 -0800

    semantic executor

--- source/main/py/helper_parser.py
+++ source/main/py/helper_parser.py
@@ -194,12 +194,13 @@ class ParserQuery(RunInference):
             prompt = self.get_prompt(query_english, schema_sql, 
                                         enum_values, fewshot_examples)
             query_sql = self.run_inference(prompt)
-            print("QUERY_SQL=>" + str(query_sql))            
-            responses = self.db_cursor.execute(query_sql)
-            responses = [row for row in responses]
-            if len(responses) > 0:
-                user_state, result_items = self.new_response(query_sql, columns,
-                                                            responses, n)
+            if query_sql != "" and len(query_sql.split("WHERE")>1):
+                print("QUERY_SQL=>" + str(query_sql))            
+                responses = self.db_cursor.execute(query_sql)
+                responses = [row for row in responses]
+                if len(responses) > 0:
+                    user_state, result_items = self.new_response(query_sql, columns,
+                                                                responses, n)
         except Exception as e:
             # print("INVOKE_ERROR=" + str(e) + "... QUERY_SQL=" + str(query_sql))
             pass 
