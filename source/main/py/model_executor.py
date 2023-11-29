0f5d6a1aa05d28c16cc065f91de60fd5e8d42f6d
Pablo <edu@pablotech.uno>
2023-11-29 10:07:58 -0800

    semantic executor

--- source/main/py/model_executor.py
+++ source/main/py/model_executor.py
@@ -86,7 +86,7 @@ class ModelExecutor(QueryExecutor):
                     time_end = time.time()
                     if isinstance(qna_model, langchain.chat_models.ChatOpenAI):
                         model_answer = model_answer.content
-                    # print(f"""qna_model={type(qna_model)} executable_name={executable_name} payload={model_payload} model_answer={model_answer}""")
+                    print(f"""qna_model={type(qna_model)} executable_name={executable_name} payload={model_payload} model_answer={model_answer}""")
                     model_answers[executable_name] = model_answer
                     model_latency[executable_name] = "{:0.2f}".format(time_end-time_start)                    
                 except Exception as e:
@@ -109,13 +109,16 @@ class QueryExecutor(ModelExecutor):
         final_state = set()
         final_items = []
         for answered_content in answered_contents:
-            for answers in answered_content.model_answers.values():
-                # print(f"""domain=>{domain} answers={answers}""")
-                for state_items in answers:
-                    user_state = state_items['user_state']
-                    if  user_state != '':
-                        final_state.add(user_state)
-                    state_items = state_items['result_items']
-                    if len(state_items) > 0:
-                        final_items.append(state_items)
+            try:
+                for domain, answers in answered_content.model_answers.items():
+                    print(f"""domain=>{domain} answers={answers}""")
+                    for state_items in answers:
+                        user_state = state_items['user_state']
+                        if  user_state != '':
+                            final_state.add(user_state)
+                        state_items = state_items['result_items']
+                        if len(state_items) > 0:
+                            final_items.append(state_items)
+            except Exception as e:
+                print("DOMAIN_EXECUTION_ERROR="+str(e))
         return final_state, final_items
