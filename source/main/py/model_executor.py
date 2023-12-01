1742c647630dc816f5be18efe6874350125bf7d7
Pablo <edu@pablotech.uno>
2023-12-01 05:37:34 -0800

    payload parallelization

--- source/main/py/model_executor.py
+++ source/main/py/model_executor.py
@@ -19,25 +19,26 @@ class ExecutionPlayload():
     def __init__(self, 
                  model_payload,
                  executable_names, 
-                 payload_id=str(uuid.uuid4()),
-                 content_id=str(uuid.uuid4())):
+                 payload_id=str(uuid.uuid4())):
         self.model_payload = model_payload
-        self.executable_names = executable_names
+        self.executable_models = { str(uuid.uuid4()): e for e in executable_names }
         self.payload_id = payload_id
-        self.content_id = content_id
 
     def get_model_payload(self):
         return self.model_payload
     
-    def get_executable_names(self):
-        return self.executable_names
-
     def get_payload_id(self):
         return self.payload_id
-
-    def get_content_id(self):
-        return self.content_id
     
+    def get_executable_ids(self):
+        return list(self.executable_models.keys())
+    
+    def get_executable_name(self, key):
+        return self.executable_models[key]
+    
+    def get_executable_count(self):
+        return len(self.executable_models)
+
 
 class QueryExecutor():
 
@@ -48,22 +49,28 @@ class QueryExecutor():
     def max_workers(self, execution_payloads):
         max_workers = 5
         for payload in execution_payloads:
-            max_workers += len(payload.get_executable_names()) 
+            max_workers += len(payload.get_executable_count()) 
+        print("max_workers=" + str(max_workers))
         return max_workers            
 
     def execute_payloads(self, execution_payloads):
         payload_answers = {}
         if len(execution_payloads) == 0:
-            print("NOTHING_TO_DO... count=" + str(len(execution_payloads)))
+            print("NOTHING_TO_DO...")
             return []
         max_workers = self.max_workers(execution_payloads)        
         with concurrent.futures.ThreadPoolExecutor(max_workers) as thread_executor:
             for execution_payload in execution_payloads:
-              try:
-                  thread_executor.submit(self.payload_answers_func, execution_payload, payload_answers)
-              except:
-                  print("UNABLE_TO_THREAD=" + str(execution_payload.content_context))
+                self.execute_payload(thread_executor, execution_payload, payload_answers)
         return list(payload_answers.values())
+    
+    def execute_payload(self, thread_executor:concurrent.futures.ThreadPoolExecutor, 
+                        execution_payload:ExecutionPlayload, payload_answers):
+        try:
+            thread_executor.submit(self.payload_answers_func, execution_payload, payload_answers)
+        except:
+            print("UNABLE_TO_THREAD=" + str(execution_payload.get_payload_id()))
+
 
 
 class ModelExecutor(QueryExecutor):
@@ -76,9 +83,9 @@ class ModelExecutor(QueryExecutor):
                               payload_answers):
         model_answers = {}
         model_latency = {}
-        for executable_name in execution_payload.get_executable_names():
-                try:
-                    
+        for executable_id in execution_payload.get_executable_ids():
+                try: 
+                    executable_name = execution_payload.get_executable_name()
                     qna_model = self.model_factory.new_model(executable_name)
                     model_payload = execution_payload.get_model_payload()
                     time_start = time.time()
@@ -87,8 +94,8 @@ class ModelExecutor(QueryExecutor):
                     if isinstance(qna_model, langchain.chat_models.ChatOpenAI):
                         model_answer = model_answer.content
                     # print(f"""qna_model={type(qna_model)} executable_name={executable_name} payload={model_payload} model_answer={model_answer}""")
-                    model_answers[executable_name] = model_answer
-                    model_latency[executable_name] = "{:0.2f}".format(time_end-time_start)                    
+                    model_answers[executable_id] = model_answer
+                    model_latency[executable_id] = "{:0.2f}".format(time_end-time_start)                    
                 except Exception as e:
                     print("ERROR_COMPOSING_ANSWER=" + str(e))
 
