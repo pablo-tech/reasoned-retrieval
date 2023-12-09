faf9127d93b4367e0d6f61b7a508a8958b9f6bdf
Pablo <edu@pablotech.uno>
2023-12-09 05:48:59 -0800

    encyclopedia interface

--- source/main/py/tool_wikipedia.py
+++ source/main/py/tool_wikipedia.py
@@ -20,12 +20,12 @@ class WikipediaSearchReader(WikipediaRetriever):
     def __init__(self, completion_llm, is_verbose=False):
         super().__init__(completion_llm, is_verbose)
 
-    def run(self, tool_input, user_query=""):
+    def run(self, tool_input="", user_query="", query_filter={}):
         return self.invoke(tool_input, self.select)
 
-    def select(self, query):
-        results = self.subquery(query)
-        return self.answer(self.summarize(results, query), query)
+    def select(self, query_txt, query_filter):
+        results = self.subquery(query_txt)
+        return self.answer(self.summarize(results, query_txt), query_txt)
 
     def subquery(self, query, k=5, n=5):
         results = []
@@ -52,11 +52,11 @@ class WikipediaLookupReader(WikipediaRetriever):
     def __init__(self, completion_llm, is_verbose=False):
         super().__init__(completion_llm, is_verbose)
 
-    def run(self, tool_input, user_query=""):
+    def run(self, tool_input="", user_query="", query_filter={}):
         return self.invoke(tool_input, self.select)
 
-    def select(self, query):
-        return self.answer(self.summarize(self.subquery(query)))
+    def select(self, query_txt, query_filter):
+        return self.answer(self.summarize(self.subquery(query_txt)))
 
     def answer(self, results):
         return [result for result in results]
